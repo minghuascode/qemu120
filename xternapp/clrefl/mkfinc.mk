0baef1cc56b981393b3b17f47229bedd77cb9bb2
minghuascode <minghuascode@localhost.localdomain>
2012-11-17 13:58:14 -0800

    update makefile to link

--- xternapp/clrefl/mkfinc.mk
+++ xternapp/clrefl/mkfinc.mk
@@ -5,13 +5,15 @@ LLVM_BUILD_PATH = $(HOME)/clang/bin-install
 LLVM_BIN_PATH = $(LLVM_BUILD_PATH)/bin
 
 LLVM_LIBS=core mc
-LLVM_CONFIG_FLAGS = $(shell $(LLVM_BIN_PATH)/llvm-config --cxxflags --ldflags \
-                                        --libs $(LLVM_LIBS))
+LLVM_CONFIG_CC_FLAGS = $(shell $(LLVM_BIN_PATH)/llvm-config --cxxflags)
+LLVM_CONFIG_LD_FLAGS = $(shell $(LLVM_BIN_PATH)/llvm-config --ldflags --libs $(LLVM_LIBS))
 
 
 CFLAGS := -fno-rtti -I$(TOPROOT)/src -I$(TOPROOT)/inc 
 CFLAGS += -I$(LLVM_BUILD_PATH)/include
-CFLAGS += $(LLVM_CONFIG_FLAGS)
+CFLAGS += $(LLVM_CONFIG_CC_FLAGS)
+
+LDFLAGS := $(LLVM_CONFIG_LD_FLAGS)
 
 
 OBJDIR := $(TOPROOT)/mkobj-dir
@@ -32,7 +34,7 @@ SRCOBJDIR := $(OBJDIR)/$(SRCDIR)
 OBJS := $(SRCS:%.cpp=%.o)
 
 
-.phony: allsub cleansub
+.phony: allsub allsubexe cleansub
 
 allsub: prelink $(OBJS:%=$(SRCOBJDIR)/%)
 
@@ -40,10 +42,48 @@ $(SRCOBJDIR)/%.o: %.cpp
 	@echo CC: $<
 	g++ $(CFLAGS) -c $< -o $@
 
+allsubexe: prelink $(OBJS:%=$(SRCOBJDIR)/%)
+	@echo EXE: $<
+	g++ \
+	        $(OBJS:%=$(SRCOBJDIR)/%) \
+	        $(SCANLIBS)  \
+	        $(CFLAGS) \
+	        $(CLANGLIBS) \
+	        $(LDFLAGS) \
+	        -o $(SRCDIR:%=$(SRCOBJDIR)/%) \
+
 cleansub:
 	@echo RM: $(SRCOBJDIR)
 	@-rm -r $(SRCOBJDIR)
 
 prelink:
 	@if [ ! -d $(SRCOBJDIR) ]; then mkdir -p $(SRCOBJDIR); echo MKDIR: $(SRCOBJDIR); fi
+
+#for linking clReflectScan: clReflectCore clReflectCpp
+SCANLIBS := $(wildcard $(OBJDIR)/clReflectCore/*.o)
+SCANLIBS += $(wildcard $(OBJDIR)/clReflectCpp/*.o)
+#for linking clReflectTest: clReflectUtil
+SCANLIBS += $(wildcard $(OBJDIR)/clReflectUtil/*.o)
+
+#from scan cmake file:
+#CLANGLIBS := -lclangParse
+#CLANGLIBS += -lclangFrontend
+#CLANGLIBS += -lclangSema
+#CLANGLIBS += -lclangAnalysis
+#CLANGLIBS += -lclangLex
+#CLANGLIBS += -lclangBasic
+#CLANGLIBS += -lclangSerialization
+#CLANGLIBS += -lclangDriver
+#CLANGLIBS += -lclangAST
+#CLANGLIBS += -lclangEdit
+ 
+#from clang/tools/driver/makefile:
+
+CLANGLIBS = \
+  -lclangFrontendTool -lclangFrontend -lclangDriver \
+  -lclangSerialization -lclangCodeGen -lclangParse \
+  -lclangSema -lclangStaticAnalyzerFrontend \
+  -lclangStaticAnalyzerCheckers -lclangStaticAnalyzerCore \
+  -lclangAnalysis -lclangARCMigrate -lclangRewrite \
+  -lclangEdit -lclangAST -lclangLex -lclangBasic
 
