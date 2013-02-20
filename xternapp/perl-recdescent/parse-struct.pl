946ddb09951f3c47fb5526068cf2d920f3570762
minghuascode <minghuascode@localhost.localdomain>
2013-02-20 08:42:23 -0800

    merged array block. to merge fields.

--- xternapp/perl-recdescent/parse-struct.pl
+++ xternapp/perl-recdescent/parse-struct.pl
@@ -23,21 +23,44 @@ fields: valuefields[$arg[0]] | arrayfields[$arg[0]]
     { print " fields: ", scalar(@{$item[1]}), " \n"; }
 
 valuefields: valuefield[$arg[0]](s /,/)
-    { { my @arrs = @{$item[1]};
-        my $r = [];
-        for(my $i=0; $i<=$#arrs; $i++) {
-            my @a = @{$arrs[$i]};
-            if ( $a[0] =~ m"field" ) {
-                push @{$r}, ["field", 
-                            sprintf("%s[%d].%s", $arg[0], $i, $a[1]), $a[2]];
-                printf(" value %s[%d].%s\n", $arg[0], $i, $a[1]);
-            }
-        }
+    { { #my @arrs = @{$item[1]};
+        my $r = ["block", $item[1]];
+        #for(my $i=0; $i<=$#arrs; $i++) {
+        #    my @a = @{$arrs[$i]};
+        #    if ( $a[0] =~ m"field" ) {
+        #        push @{$r}, ["field", 
+        #                    sprintf("%s[%d].%s", $arg[0], $i, $a[1]), $a[2]];
+         #       printf(" value %s[%d].%s\n", $arg[0], $i, $a[1]);
+        #    }
+        #}
         $return = $r; }
     }
 
 valuefield: identifier '=' value[$arg[0].".$item[1]"]
-    { $return = ["field", "$arg[0].$item[1]", "$item[3]"]; }
+    {   my @a = @{$item[3]};
+        if ( $a[0] =~ m"scalar" ) {
+            $return = ["field", "$arg[0].$item[1]", "$item[3]"]; 
+        } elsif ( $a[0] =~ m"block" ) {
+
+            #todo: return block, and make parent to merge up
+            my $r = [];
+            #my @b = @{$a[1]};
+            #for (my $j=0; $j < scalar(@b); $j++) {
+            #    my @aa = @{$b[$j]};
+            #    if ( $aa[0] =~ m"field" ) {
+            #        push @{$r}, ["field", "$aa[1]", $aa[2]];
+            #        printf(" field %s\n", $aa[1]);
+            #    } else {
+            #        print STDERR "Error unknown $aa[0] in field block\n";
+            #    }
+            #}
+            $return = $item[3];
+
+        } else {
+            print STDERR "Error unknown $a[0] in field\n";
+            $return = ["field", "Error unknown $a[0] in field", ""];
+        }
+    }
 
 arrayfields: arrayfield[""](s /,/)
     { { my @arrs = @{$item[1]};
@@ -47,13 +70,25 @@ arrayfields: arrayfield[""](s /,/)
             if ( $a[0] =~ m"scalar" ) {
                 push @{$r}, ["field", "$arg[0].[$i]", $a[1].$a[2]];
                 printf(" array %s[%d]\n", $arg[0], $i);
-            } 
-            if ( $a[0] =~ m"field" ) {
+            } elsif ( $a[0] =~ m"field" ) {
                 push @{$r}, ["field", "$arg[0].[$i].$a[1]", $a[2]];
                 printf(" array %s[%d].%s\n", $arg[0], $i, $a[1]);
+            } elsif ( $a[0] =~ m"block" ) {
+                my @b = @{$a[1]};
+                for (my $j=0; $j < scalar(@b); $j++) {
+                    my @aa = @{$b[$j]};
+                    if ( $aa[1] =~ m"field" ) {
+                        push @{$r}, ["field", "$arg[0].[$i].$aa[1]", $aa[2]];
+                        printf(" array %s[%d].%s\n", $arg[0], $i, $aa[1]);
+                    } else {
+                        print STDERR "Error unknown $aa[0] in array block\n";
+                    }
+                }
+            } else {
+                print STDERR "Error unknown $a[0] in array\n";
             }
         }
-        $return = $r; }
+        $return = ["block", $r]; }
     }
 
 arrayfield: value[$arg[0]]
