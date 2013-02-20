9c26f5dccbe79174f31f9854220ab8b6f746e8b4
minghuascode <minghuascode@localhost.localdomain>
2013-02-19 23:40:14 -0800

    use rule local var

--- xternapp/perl-recdescent/parse-struct.pl
+++ xternapp/perl-recdescent/parse-struct.pl
@@ -21,36 +21,45 @@ fields: singlefield[$arg[0]] secondfield[$arg[0]](s?)
 
 secondfield: ',' singlefield[$arg[0]]
 
-singlefield: arrayfield[$arg[0]] | valuefield[$arg[0]]
+singlefield: arrayfields[$arg[0]] | valuefield[$arg[0]]
     { print " field: $item[1] \n"; }
 
 valuefield: identifier '=' value[$arg[0].".$item[1]"]
     { $return = " identifier = $arg[0].$item[1] $item[3] "; }
 
-arrayfield: value[$arg[0].".[]"]
-    { $return = "array = $arg[0].[] "; }
+arrayfields: <rulevar: local $count = 0>
+arrayfields: arrayfield[$arg[0]](s)
+
+arrayfield: value[$arg[0].".[".$count."]"]
+    { $return = "array = $arg[0].[".$count."] "; 
+      $thisrule->{"local"}{"count"}++; }
 
 value: charvalue | datavalue | stringvalue | block | enumvalue
     { $return = $item[1]; }
 
 datavalue: /[019]/
-    { print "datavalue: $item[1]\n"; $return = " datavalue $item[1] "; }
+    { #print "datavalue: $item[1]\n"; 
+      $return = " datavalue $item[1] "; }
 
 charvalue: /0/ /\\'\\\\000\\'/
-    { print "charvalue: $item[1] $item[2]\n"; $return = " charvalue 0 "; } 
+    { #print "charvalue: $item[1] $item[2]\n"; 
+      $return = " charvalue 0 "; } 
 
 stringvalue: stringvalue1 | stringvalue2
  
 stringvalue1: /\\'\\\\000\\'/ /<repeats/ /[0-9]+/ /times>/
-    { print "stringvalue: \n"; $return = " str1 "; } 
+    { #print "stringvalue: \n"; 
+      $return = " str1 "; } 
 
 stringvalue2: /\\"\\\\000[\\\\0]+\\"/
-    { print "stringvalue: \n"; $return = " str2 "; } 
+    { #print "stringvalue: \n"; 
+      $return = " str2 "; } 
 
 enumvalue: /\\w+::\\w+/
 
 identifier: /[a-zA-Z_]\\w*/
-    { print "identifier $item[1]\n"; $return = $item[1]; }
+    { #print "identifier $item[1]\n"; 
+      $return = $item[1]; }
 
 };
 
