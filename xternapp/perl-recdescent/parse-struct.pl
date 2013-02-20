4d75f094518a40ffbfaa05460b400006f8dc468d
minghuascode <minghuascode@localhost.localdomain>
2013-02-20 01:13:25 -0800

    construct the tree, partial work

--- xternapp/perl-recdescent/parse-struct.pl
+++ xternapp/perl-recdescent/parse-struct.pl
@@ -4,6 +4,8 @@
 use strict;
 use Parse::RecDescent;
 
+$::RD_HINT = 1;
+
     sub dbg1{printf("dbg1 %s\n", shift);}
     sub dbg2{printf("dbg2 %s %s\n", shift, substr(shift,0,10));}
 
@@ -15,47 +17,71 @@ startrule: '$1' '=' block["top"]
     { print "$item[0] $item[3] \n"; }
 
 block: '{' fields[$arg[0]] '}'
-    { $return = " block $item[2] "; }
+    { $return = $item[2]; }
 
-fields: singlefield[$arg[0]] secondfield[$arg[0]](s?)
+fields: valuefields[$arg[0]] | arrayfields[$arg[0]]
+    { print " fields: ", scalar(@{$item[1]}), " \n"; }
 
-secondfield: ',' singlefield[$arg[0]]
-
-singlefield: arrayfields[$arg[0]] | valuefield[$arg[0]]
-    { print " field: $item[1] \n"; }
+valuefields: valuefield[$arg[0]](s /,/)
+    { { my @arrs = @{$item[1]};
+        my $r = [];
+        for(my $i=0; $i<=$#arrs; $i++) {
+            my @a = @{$arrs[$i]};
+            if ( $a[0] =~ m"field" ) {
+                push @{$r}, ["field", 
+                            sprintf("%s[%d].%s", $arg[0], $i, $a[1]), $a[2]];
+                printf(" value %s[%d].%s\n", $arg[0], $i, $a[1]);
+            }
+        }
+        $return = $r; }
+    }
 
 valuefield: identifier '=' value[$arg[0].".$item[1]"]
-    { $return = " identifier = $arg[0].$item[1] $item[3] "; }
+    { $return = ["field", "$arg[0].$item[1]", "$item[3]"]; }
 
-arrayfields: <rulevar: local $count = 0>
-arrayfields: arrayfield[$arg[0]](s)
+arrayfields: arrayfield[""](s /,/)
+    { { my @arrs = @{$item[1]};
+        my $r = [];
+        for(my $i=0; $i < scalar(@arrs); $i++) {
+            my @a = @{$arrs[$i]};
+            if ( $a[0] =~ m"scalar" ) {
+                push @{$r}, ["field", "$arg[0].[$i]", $a[1].$a[2]];
+                printf(" array %s[%d]\n", $arg[0], $i);
+            } 
+            if ( $a[0] =~ m"field" ) {
+                push @{$r}, ["field", "$arg[0].[$i].$a[1]", $a[2]];
+                printf(" array %s[%d].%s\n", $arg[0], $i, $a[1]);
+            }
+        }
+        $return = $r; }
+    }
 
-arrayfield: value[$arg[0].".[".$count."]"]
-    { $return = "array = $arg[0].[".$count."] "; 
-      $thisrule->{"local"}{"count"}++; }
+arrayfield: value[$arg[0]]
+    { $return = $item[1]; }
 
 value: charvalue | datavalue | stringvalue | block | enumvalue
     { $return = $item[1]; }
 
 datavalue: /[019]/
     { #print "datavalue: $item[1]\n"; 
-      $return = " datavalue $item[1] "; }
+      $return = ["scalar", "data", $item[1]]; }
 
 charvalue: /0/ /\\'\\\\000\\'/
     { #print "charvalue: $item[1] $item[2]\n"; 
-      $return = " charvalue 0 "; } 
+      $return = ["scalar", "char", "0"]; } 
 
 stringvalue: stringvalue1 | stringvalue2
  
 stringvalue1: /\\'\\\\000\\'/ /<repeats/ /[0-9]+/ /times>/
     { #print "stringvalue: \n"; 
-      $return = " str1 "; } 
+      $return = ["scalar", "string", "str1"]; } 
 
 stringvalue2: /\\"\\\\000[\\\\0]+\\"/
     { #print "stringvalue: \n"; 
-      $return = " str2 "; } 
+      $return = ["scalar", "string", " str2 "]; } 
 
 enumvalue: /\\w+::\\w+/
+    { $return = ["scalar", "enum", $item[1]]; }
 
 identifier: /[a-zA-Z_]\\w*/
     { #print "identifier $item[1]\n"; 
