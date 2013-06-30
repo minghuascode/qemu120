757e518e7b7bcf0a4c0bc598823e99444c5f17f5
minghuascode <minghuascode@localhost.localdomain>
2013-06-29 19:36:30 -0700

    sanm4jsample init directory and files

---
+++ xternapp/snmp4jsample/test1src/src/s4jclient/S4jclient.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s4jclient;
+
+/**
+ *
+ * @author me
+ */
+public class S4jclient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
